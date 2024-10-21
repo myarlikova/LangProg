4dc2daa9320194eb00d7b4467ffa10c499e4067c
yarlik <myarlikova1917@gmail.com>
2024-10-21 16:39:07 +0700

    Автопреобразование данных

--- pr001.java
+++ pr001.java
@@ -23,6 +23,14 @@ class pr001 {
 		//вещественные типы данных
 		float f; //32
 		double dd; //64
+		sh = b;
+		b = (byte)sh;
+		dd = 1.5;
+		intg =(int)dd;
+		intg = (double)10/3;
+		f = intg;
+		System.out.println(intg + " " + dd);
+
 	        //Символьный тип данных
 		char ch;//16
 		ch = 'A';
@@ -30,10 +38,18 @@ class pr001 {
 		for (int i=0; i<68; i++)
 			System.out.print(ch++ + " ");
 		System.out.println();
+		ch ='0';
+		System.out.println(((int)ch));
 		// Логический тип
-		boolean bool;
+		boolean bool; bool2 = false;
 		bool = true;
-		bool = false;
+		System.out.println(!bool);
+		System.out.println(bool || bool2);
+		System.out.println(bool && bool2);
+		System.out.println(bool ^ bool2);
+		System.out.println("Перевод  строки\n" + "Табуляция\t" + "\"Кавычки\" в строках" + (!bool && (bool2 | bool)));
+		ch = '\'';
+
 
 	}
 
