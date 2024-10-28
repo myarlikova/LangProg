9bdfa33a2eadfd07b15ca763405cfeefbe08c389
yarlik <myarlikova1917@gmail.com>
2024-10-28 16:50:07 +0700

    Объекты, неправильный вариант

---
+++ pr003.java
@@ -0,0 +1,27 @@
+class Cars {
+	int pass;// количество пассажиров
+	int volume; //объем бака
+	double fuel;//расход топлива в литрах
+}
+
+clas pr003 {
+	public static void main(String[] args) {
+		Cars Honda = new Cars();
+		Honda.pass = 2;
+		Honda.volume = 55;
+		Honda.fuel = 20.0;
+		Cars = BMW = new Cars();
+		BMW.pass = 5;
+		BMW.volume = 40;
+		BMW.fuel = 15.5;
+
+		System.out.println("Параметры Хонды:");
+		System.out.println("Колмичество пассажиров: " + Honda.pass + "Объём бака" + Honda.volume + "Расход топлива" + Honda.fuel);
+
+		double range;
+		range = Honda.volume/Honda.fuel*100;
+		System.out.println("Хонда на полном баке проедет" + range + "км");
+		range = BMW.volume/BMW.fuel*100;
+		System.out.println("BMW на полном баке проедет" + range + "км");
+	}
+}
