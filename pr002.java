648faef4d7b2f3d5d5f50123ae0ba19eaa366335
yarlik <myarlikova1917@gmail.com>
2024-10-28 15:38:39 +0700

    Добавили break и continue

--- pr002.java
+++ pr002.java
@@ -68,11 +68,86 @@ class pr002 {
 
 		System.out.println("\n");
 		//цикл do while
-		char ch2;
+/*		char ch2;
 		do {
 			 System.out.print("введите символ и нажмите \"RETURN\": ");
 			 ch2 = (char) System.in.read();
 		} while (ch2 != 'q');
+*/
+		//оператор breaak
+		for(int i=0;i<100;i++)
+			System.out.println("Значение i;" +i);
+			if (i == 5)
+				break;
+			System.out.println("Следущая итерация");
+		}
+		System.out.println();
 
+		for(int i=0; i<3; i++) {
+			System.out.println("Счетчик внешнего цикла: " + i);
+			System.out.println("Счетчик внутреннего цикла: ");
+			int t = 0;
+			while(t<100) {
+				if(t==10)
+					break;
+				System.out.println(t + "");
+				t++;
+			}
+			System.out.println();
+		}
+		//использование break с меткой
+		System.out.println();
+		int i2;
+		for(i2=1; i2<4;i2++) {
+one:		{
+two:			{
+three:				{
+					System.out.println("\ni2 равно " +i2);
+					if(i2==1) break one;
+					if(i2==2) break two;
+					if(i2==3) break three;
+
+					System.out.println("Не выполняется");
+				}
+				System.out.println("После блока three");
+			}
+			System.out.println("После блока two");
+		  }
+		  System.out.println("После блока one");
+		}
+		System.out.println("После цикла for");
+
+		//еще один break с меткой
+		System.out.println();
+done:
+		for(int i=0; i<10;i++) {
+			for(int j=0; j<10;j++) {
+				for(int k=0; k<10;k++) {
+					System.out.println(k + " ");
+					if (k == 5) break done;
+				}
+				System.out.println("После цикла k");
+			}
+			System.out.println("После цикла j");
+		}
+		System.out.println("После цикла i");
+
+		//оператор continue
+		for (int i=0; i<=100; i++) {
+				if((i%2) !=0)
+					continue;
+				System.out.println(i);
+		}
+outerloop:
+		for (int i=1; i<=10; i++) {
+			System.out.println("\nпроход внешнего цикла №" + i + " , внутренний цикл: ");
+			for (int j=1; j<=10; j++) {
+				if(j==5) continue outerloop;
+				System.out.println(j);
+			}
+		}
+		System.out.println();
 	}
+
+
 }
