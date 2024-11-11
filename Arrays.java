cbf1586b14c552e8c6179088cd3a86f3d5a466ac
yarlik <myarlikova1917@gmail.com>
2024-11-11 15:28:32 +0700

    Добавили двумерные массивы, ссылки на массивы и переменную length

--- Arrays.java
+++ Arrays.java
@@ -25,22 +25,161 @@ class Arrays {
 	//Находим максимальное и минимальное значение в массиве
 	int min, max;
 	min = max =nums[0];
-	for(int i = 0; i < 10; i++){
-		if(nums[i] < min) = nums[i];
-		if(nums[i] > max) = nums[i];
+	for(int i = 0; i < nums.length; i++){
+		if(nums[i] < min) min = nums[i];
+		if(nums[i] > max) max = nums[i];
 	}	
-	System.out.println("Min: " + min"\nMax: " + max));
+	System.out.println("Минимальное значение: " + min + "\nМаксимальное значение: " + max);
 	System.out.println();
 
 	//Альтернативная форма инициализации массива:
 	int x = 10;
 	int[] nums1 ={32, -42, 1132, 53,124,94,x,-522,-10,11};
-	min = max =nums[0];
-        for(int i = 1; i < 10; i++){
-                if(nums[i] < min) = nums1[i];
-                if(nums[i] > max) = nums1[i];
+	min = max =nums1[0];
+        for(int i = 1; i < nums1.length; i++){
+                if(nums1[i] < min) min = nums1[i];
+                if(nums1[i] > max) max = nums1[i];
         }
-        System.out.println("Min: " + min"\nMax: " + max);
+        System.out.println("Минимальное значение: " + min + "\nМаксимальное значение: " + max);
         System.out.println();
+
+	//двумерные массивы
+	int t, j;
+	int [][] table= new int[3][4];
+	for (t = 0; t < 3; t++){
+		for (j = 0; j < 3; j++) {
+			table[t][j] = (t*4) + j + 1;
+			System.out.println(table[t][j] + "\t");
+		}
+		System.out.println();
+	}
+	System.out.println();
+
+	//непрямоугольные массивы
+	int[][]	riders = new int[7][];
+	riders[0] = new int[10];
+	riders[1] = new int[10];
+	riders[2] = new int[10];
+	riders[3] = new int[10];
+	riders[4] = new int[10];
+	riders[5] = new int[2];
+	riders[6] = new int[2];
+
+	//Заполнение массива
+
+	for(int i = 0; i < 5; i++) 
+		for(int k = 0; k < 10; k++)
+			riders[i][k] = i+ k + 10;
+	for(int i = 5; i < 7; i++)
+		for(int k = 0; k < 2; k++)
+			riders[i][k] = i+ k + 10;
+	//Вывод массива
+	System.out.println("перевкезено пассажиров в будние дни: ");
+	for(int i = 0; i < 5; i++) {
+                for(int k = 0; k < 10; k++)
+                       System.out.println(riders[i][k] + " "); 
+		System.out.println();
+	}
+	System.out.println();
+	System.out.println("перевeзено пассажиров в выходные дни: ");
+        for(int i = 5; i < 7; i++) {
+                for(int k = 0; k < 2; k++)
+                       System.out.println(riders[i][k] + " ");
+		System.out.println();
+	}
+	System.out.println();
+
+	//Инициализация двумерного массива
+	int [][] sqrs = {
+		{1, 1},
+		{2, 4},
+		{3, 9},
+		{4, 16},
+		{5, 25},
+		{6, 36},
+                {7, 49},
+                {8, 64},
+                {9, 81},
+                {10, 100}
+
+	};
+
+	System.out.println("Содержимое двумерного массива sqrs: ");
+        for(int i = 0; i < 10; i++) {
+                for(int k = 0; k < 2; k++)
+                       System.out.println(riders[i][k] + "\t ");
+		System.out.println();
+        }
+
+	//Объявление массива int[]nums = new nums[3];
+	//Альтернативный вариант int nums[]  = new nums [];
+	
+	//ссылочные переменные указывающие на массивы
+	int[]  nums2 = new int[10];
+	int[]  nums3 = new int[10];
+
+	for(int i = 0; i < 10; i++) {
+		nums2[i] = i;
+                nums3[i] = i;
+	}
+
+	System.out.println("Массив nums2: ");
+	for(int i = 0; i < 10; i++) {
+		System.out.print(nums2[i] + " ");
+	System.out.println();
+
+	System.out.println("Массив nums3: ");
+        for(int i = 0; i < 10; i++) {
+                System.out.print(nums3[i] + " ");
+        System.out.println();
+	
+	nums2[3] = 99;
+
+	System.out.println("nums2 и ");
+	System.out.print()
+	for(int i = 0; i < 10; i++) 
+		System.out.println("Изменяется значение только у nums2[]: ")
+	System.out.println();
+
+	System.out.println("Изменяется значение только у nums2[]: ");
+        System.out.print()
+        for(int i = 0; i < 10; i++)
+		
+	System.out.println();
+        System.out.println();
+	nums3=nums2;
+
+
+	System.out.println("Массив nums2: ");
+        for(int i = 0; i < 10; i++) {
+                System.out.print(nums2[i] + " ");
+        System.out.println();
+
+        System.out.println("Массив nums3: ");
+        for(int i = 0; i < 10; i++) {
+                System.out.print(nums3[i] + " ");
+        System.out.println();
+
+	//Переменная экземпляра
+	int[] list =  new int[10];
+	int nums4 = {1, 2, 3};
+	int [][] table1 = {
+		{1, 2, 3}
+		{4, 5}
+		{6, 7, 8, 9}
+		{0}
+	};
+
+	System.out.println("Длина list " + list.length);
+	System.out.println("Длина nums4 " + nums4.length);
+	System.out.println("Длина table " + table1.length);
+	System.out.println("Длина table1[0] " + table1[0].length);
+        System.out.println("Длина table1[1] " + table1[1].length);
+        System.out.println("Длина table1[2] " + table1[2].length);
+	System.out.println("Длина table1[3] " + table1[3].length);
+	}
+
+	}
 }
 
+}
