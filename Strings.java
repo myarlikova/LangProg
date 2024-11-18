2e618bfb1f0b4fbd770e43fe6f572ba77a771ea1
yarlik <myarlikova1917@gmail.com>
2024-11-18 13:42:46 +0700

    Добавили методы работы со строками, массивы строк, аргументы коммандной строки

--- Strings.java
+++ Strings.java
@@ -28,8 +28,108 @@ class Strings {
 		System.out.println("В строке под индексом 3: " + ch);
 
 		for (int i = 0; i < length(); i++)
-			System.out.println((str.charAt(i))
+			System.out.println((str.charAt(i));
 		System.out.println();
+
+		System.out.println();
+
+		//Сравнение строк 
+		int result;
+		result = str.compareTo("строка номер одно");
+		System.out.println("Результат сравнения строк str2 и  str " + result);
+
+		result = str.compareTo(str3);
+                System.out.println("Результат сравнения строк str3 и  str " + result);
+
+		//Поиск подстроки и вывод индекса по первому вхождению подстроки
+		str3 = "од";
+		result = str.indexOf(str3);
+		System.out.println();
+
+		System.out.println("Первое вхождение подстроки \"од\" в str: " + result);
+
+		//Поиск подстроки и вывод индекса по первому вхождению подстроки
+		str3 = "3";
+		result = str.lastIndexOf(str3);
+                System.out.println("Последнее вхождение подстроки \"три\" в str: " + result);
+
+		str3 = "н";
+                result = str.lastIndexOf(str3);
+                System.out.println("Последнее вхождение подстроки \"н\" в str: " + result);
+
+		//массивы строк
+		System.out.println();
+		String[] strs = {"Разные", "элементы", "массива","строк"};
+		System.out.println("Исходный массив: ");
+		for(String s : strs)
+			System.out.println(s + " ");
+		System.out.println("\n");
+
+		strs[1] = "измененный элемент";
+		strs[4] = "Новая строка один";
+
+		System.out.println("Измененный массив: ");
+                for(String s : strs)
+                        System.out.println(s + " ");
+                System.out.println("\n");
+
+		//Создание подстроки 
+		String substr;
+		System.out.println("Выделяем подстроку из строки str: ");
+		substr = str.substring(13,16);
+		System.out.println("Оригинальная строка: " + str);
+		System.out.println("Подстрока: " + substr);
+
+		//Управление оператором switch при помощи строк
+		String command = "cancel";
+		switch(command) {
+			case "connect":
+				System.out.println("Подключение");
+				break;
+			case "cancel":
+				System.out.println("Отмена");
+				break;
+			case "disconnect":
+                                System.out.println("Отключение");
+                                break;
+			default:
+			System.out.println("Неизвестная комманда");
+                        break;
+		}
+		System.out.println();
+
+		//Текстовые блоки
+		String strblk = """
+			Пример блока с нестандартным форматированием строк.Можно пользоваться переносом строки, табуляцией и прочими управляющими символами.
+			""";
+		System.out.println(strblk);
+
+		//Аргументы коммандной строки
+		System.out.println("Количество параметров, переданных программе: " + args.length);
+		System.out.println("Список параметров: ");
+		for (int i = 0; i<args.length; i++)
+			System.out.println("Аргумент[" + i + "]: "+ args[i]);
+		System.out.println();
+
+		String [][] phoneNumbers = {
+			{"Авторемонт" "383-262-25-0"},
+			{"Деканат" "244-85-10"},
+			{"Поликлиника" "353-01-02"},
+			{"Сантехник" "206-08-11"},
+		};
+		if (args.length != 1)
+			System.out.println("Usage: java Strings <Наименование>");
+		else {
+			for (int i2=0; i2<phoneNumbers.length; i2++) {
+				if(phoneNumbers[i2][0].equals(args[0])) {
+					System.out.println(phoneNumbers[i2][0] + ":" + phoneNumbers[i2][1]);
+					break;
+				}
+			}
+			if()
+
+
+
 
 
 	}
