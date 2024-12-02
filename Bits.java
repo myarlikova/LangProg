5213e883679132013f607faec7096a82ffea0a9f
yarlik <myarlikova1917@gmail.com>
2024-12-02 15:26:55 +0700

    Пример использования внутреннего класс

--- Bits.java
+++ Bits.java
@@ -1,33 +1,46 @@
 import java.util.Random;
+class Bits {
+	public static void main (String[] args) {
 
-class ShowBits {
-	int numbits;
-	ShowBits(int n) {
-		numbits = n;
-	}
-	void show(long val) {
-		long mask = 1;
+		class ShowBits {
+			int numbits;
+			ShowBits(int n) {
+			numbits = n;
+		}
+		void show(long val) {
+			long mask = 1;
 
 		//Сдвиг влево значения 1
-		mask <<= numbits-1;
+			mask <<= numbits-1;
 
-		int spacer = 0;
-		for(; mask != 0; mask >>>= 1) {
-			if((val & mask) != 0) System.out.print("1");
-			else System.out.print("0");
-			spacer++;
-			if((spacer % 8) == 0) {
-				System.out.print(" ");
-				spacer = 0;
+			int spacer = 0;
+			for(; mask != 0; mask >>>= 1) {
+				if((val & mask) != 0) System.out.print("1");
+				else System.out.print("0");
+				spacer++;
+				if((spacer % 8) == 0) {
+					System.out.print(" ");
+					spacer = 0;
+				}
 			}
+			System.out.println();
 		}
-		System.out.println();
+		}
+		for (byte b = -128/; b <= 127; ++b ) {
+			ShowBits byteval = new ShowBits(8);
+			System.out.print(b + " в двоичном виде: ");
+			byteval.show(b);
+			if (b==127) 
+				break;
+		}
+	}
 }
-
-class Bits {
+/*
+class Bits2 {
 	public static void main (String[] args) {
 		char ch;
 		// Сброс 6-го бита - изменение регистра латиницы
+		char ch;
 		System.out.print("Преобразование букв из нижнего регистра в верхний");
 		for(int i=0; i<26; i++){
 			ch = (char) ('a' + i);
@@ -165,3 +178,4 @@ class Bits {
 	}
 
 }
+*/
