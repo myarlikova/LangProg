c8fb28406cb564ea04e6cafe7c50a3f4404cb850
yarlik <myarlikova1917@gmail.com>
2024-12-16 17:10:13 +0700

    Внешний вид реализации обобщенного интерфейса

---
+++ GenIF.java
@@ -0,0 +1,13 @@
+interface abstrIF <T> {
+        void nameMeth(T o);
+}
+class MyClass <T> implements <T> {
+        ...
+}
+
+//Возможен вариант:
+
+class MyClass implements <Double> {
+        ...
+}
+
